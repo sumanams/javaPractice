10a0201f0ca6ff2e0367681b6bea42f75956ac1d
Sumana Satyaprakash <46537756+sumana-infosys@users.noreply.github.com>
2022-01-25 19:50:47 -0500

    modified Am.java

--- src/javaPractice/Am.java
+++ src/javaPractice/Am.java
@@ -9,13 +9,18 @@ public class Am {
 		// TODO Auto-generated method stub
 		List<List<Integer>> intAr=new  ArrayList<List<Integer>>();
 			//{{1,0,0},{1,0,0},{1,9,0}};
-		System.out.println(intAr.length);
+		//System.out.println(intAr.length);
 
-
-
-		for (int i=0;i<intAr.length;i++) {
-			for (int j=0;j<intAr[i].length;j++) {
-				System.out.println(intAr[i][j]);
+		//******** Feature ***********
+		int count=0;
+		for (int i=0;i<intAr.size();i++) {
+			for (int j=0;j<intAr.get(i).size();j++) {
+				if(intAr.get(i).get(j)==1) {
+					count++;
+				}
+				if(intAr.get(i).get(j)==9) {
+					break;
+				}
 				
 			}
 		}
